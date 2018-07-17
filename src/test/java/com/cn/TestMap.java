47c58a3e9a5becb0a543482ff16659d1766d6c27
admin <admin@172.16.2.41>
2018-07-17 16:20:22 +0800

    apollo测试

---
+++ src/test/java/com/cn/TestMap.java
@@ -0,0 +1,18 @@
+package com.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/** 
+ * @author 作者 E-mail: 
+ * @version 创建时间：2018年7月16日 下午3:09:17 
+ * 类说明 
+ */
+public class TestMap {
+	public static void main(String[] args) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		map.put("name", "zhangsan");
+		
+	}
+}
